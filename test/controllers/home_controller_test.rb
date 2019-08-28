d34dbb2d8797bb800bb4de91f09dc21cca6d6e7b
Simon Cousineau <simon.cousineau99@gmail.com>
2019-08-28 09:38:09 -0400

    Setup Travis

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get home_url
     assert_response :success
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
 end
