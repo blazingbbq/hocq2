0bf39a3c482cde5afe92f1affcca1fa49f831071
Simon Cousineau <simon.cousineau99@gmail.com>
2019-08-30 08:35:54 -0400

    Added MP image resources and basic gameplay loop

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,13 +1,16 @@
 class QuizController < ApplicationController
+  NUMBER_OF_MPS = 338  # TODO: Remove magic number...
+
   def realistic
     @current_game = session[:current_game]
     @current_game ||= {
       correct: 0,
       skipped: 0,
       incorrect: 0,
-      total: 338,
-      mp_id: 1,
-  }.as_json
+      total: NUMBER_OF_MPS,
+      mp_id: generate_new,
+      seen: [],
+    }.as_json
     session[:current_game] = @current_game
   end
 
@@ -15,15 +18,37 @@ class QuizController < ApplicationController
     @current_game = session[:current_game]
     redirect_to home_url unless @current_game
 
-    @current_game["correct"] += 1
-    # @current_game["correct"] = 0 if params[:skip]
+    if params[:skip]
+      @current_game["skipped"] += 1
+    else
+      @current_game["correct"] += 1
+    end
+
+    @current_game["seen"] << @current_game["mp_id"]
+    @current_game["mp_id"] = generate_new
     session[:current_game] = @current_game
+
+    Rails.logger.info("CURRENT_GAME: #{@current_game}")
 
     render :realistic
   end
 
   def redir
-    session[:current_game] = nil  # TODO: Remove game reset on redir from /verify
+    session[:current_game] = nil  # FIXME: Remove game reset on redir from /verify
     redirect_to realistic_url
   end 
+
+  private
+
+  def generate_new
+    pool = [*0...NUMBER_OF_MPS] - (session[:current_game] ? session[:current_game]["seen"] : [])
+
+    return gameover if pool.empty?
+
+    pool.sample
+  end
+
+  def gameover
+    # TODO: Implement gameover callback
+  end
 end
